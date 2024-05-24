c383299fac9feda6bc03182a0c110b673f4ee898
JFChavezC001 <jfchavezc001@gmail.com>
2024-05-24 11:38:47 -0600

    jugamos un poco con las impresiones a pantalla, asi como el conocer los tipos de variable

--- primera-app/src/App.java
+++ primera-app/src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // una variable es un espacio en la memoria con valor cambiante
+        // las declaraciones son la asignacion de un tipo de dato y un nombre a la
+        // variable sin darle valor inicial
+        // una definicion es asignar un tipo de dato y nombre a la variable dandole o no
+        // un valor inicial
+        // una asignacion es la asignacion de un valor a una variable ya declarada
+        /*
+         * TIPOS DE VARIABLES
+         * primitivas: almacenan valores basicos
+         * de referencia: Almacenan direcciones de memoria que apuntan a objetos
+         */
+         
+        //variable primitiva
+        int numero = 5; 
+        double dec = 5.43;
+        String nombre ="Hola papu misterioso";
+        System.out.println(nombre);
+        System.out.println(dec);
+        System.out.println(numero);
+        
     }
 }
